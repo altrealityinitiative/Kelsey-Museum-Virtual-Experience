52913518d60d2ce5009ec8dfc51413a1d7857f08
Tiger Zhang <yu266426@gmail.com>
2026-04-11 15:09:10 -0400

    Revert "Merge branch 'main' into sarraf"
    This reverts commit 274db8333021ee324f67c9e054a979458c64d63a, reversing
    changes made to fea5c0149c6bac0868c4d69e803654b889a1f540.

--- src/exhibit-state.ts
+++
@@ -1,9 +0,0 @@
-export const EXHIBIT_MODEL_SPACE = "Exhibit-Model";
-
-export const ExhibitState = {
-  modelToLoad: "",
-  modelScale: 1,
-  modelRotationX: 0,
-  modelRotationY: 0,
-  modelRotationZ: 0,
-};
